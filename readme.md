88aadbd3a2febaf54cf42e3c1de06d7873edef91
CdeJong <christiaan240@gmail.com>
2021-01-23 00:16:32 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+### MobWard
+Creates mobfree zone around a block (mobward).   
+version: 1.0.0-snapshot
---
+++ src/main/java/com/shebbasoft/MobWard.java
@@ -0,0 +1,17 @@
+package com.shebbasoft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MobWard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
